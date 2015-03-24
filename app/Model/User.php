6efa075e86efde7a424be8b5b6ed657dc44d0593
umutAkk <umut.akkulak5@etu.univ-lorraine.fr>
2015-03-24 17:13:15 +0100

    fin de connection,deconnection, inscription et d'autres trucs

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,10 +1,30 @@
 <?php 
 class User extends AppModel {
-	public $hasMany = array( 'Member' => array('className' => 'Member', 'foreignKey' => 'member_id'),
-							'Message' => array('className' => 'Message', 'foreignKey' => 'message_id'),
-							'Checked' => array('className' => 'Checked', 'foreignKey' => 'checkedby_id'),
+	public $hasMany = array( 'Member' => array('className' => 'Member', 'foreignKey' => 'user_id'),
+							'Message' => array('className' => 'Message', 'foreignKey' => 'id'),
+							'Checked' => array('className' => 'Checked', 'foreignKey' => 'task_id'),
 							'Friend' => array('className' => 'Friend', 'foreignKey' => 'friend_id')
 							);
 
 	public $hasOne = 'Friend';
+        
+        public $validate = array(
+                            'username' => array(
+                                            'rule'  => 'notEmpty',
+                                            'message' => 'Entrer un pseudo !'
+                                            ),
+                            'password' => array(
+                                            'rule' => 'notEmpty',
+                                            'message' => 'Entrer un mot de passe !'
+                                             ),
+                            'email'    => array(
+                                            'rule' => 'notEmpty',
+                                            'message' => 'Entrer un email !'
+                                             ),
+                            'age'      => array(
+                                            'rule' => 'notEmpty',
+                                            'message' => 'Entrer votre date de naissance !'
+                                             )
+                            );
+         
 }
---
+++ app/View/Users/inscription.ctp
@@ -0,0 +1,8 @@
+<h3>Inscription</h3>
+<?php echo $this->Form->create('User'); ?>
+    <?php echo $this->Form->input('username', array('label' => 'nom', 'placeholder' => 'username')); ?>
+    <?php echo $this->Form->input('password', array('label' => 'mot de passe','placeholder' => 'Password')); ?>
+    <?php echo $this->Form->input('re_password', array('type'=>'password', 'label' => 'confirmation','placeholder' => 'retaper votre mot de passe')); ?>
+    <?php echo $this->Form->input('email', array('label' => 'email', 'placeholder' => 'email')); ?>
+    <?php echo $this->Form->input('age', array('label' => 'age','placeholder' => 'age')); ?>
+    <?php echo $this->Form->end('s\'inscrire'); ?>
