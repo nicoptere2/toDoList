950856367f6212491a9e5185fa52ebb7f8436e90
Nicolas <n.weissenbach@laposte.net>
2015-03-24 22:56:56 +0100

    ajout de verification dans le model User
    ca a été écrit par umut, je ne sais pas ce que ca vient faire dans cette branch...

--- app/Model/User.php
+++ app/Model/User.php
@@ -7,4 +7,24 @@ class User extends AppModel {
 							);
 
 	public $hasOne = 'Friend';
+
+    public $validate = array(
+            'username' => array(
+                'rule'  => 'notEmpty',
+                'message' => 'Entrer un pseudo !'
+                ),
+                'password' => array(
+                'rule' => 'notEmpty',
+                'message' => 'Entrer un mot de passe !'
+                ),
+                'email'    => array(
+                'rule' => 'email',
+                'message' => 'Entrer un email !'
+                ),
+                'age'      => array(
+                'rule' => 'notEmpty',
+                'message' => 'Entrer votre date de naissance !'
+                )
+        );
+
 }
