03648d2198223a862bb5e87e3029bda5ea3e265a
nicoptere <n.weissenbach@laposte.net>
2015-03-25 16:19:24 +0100

    suppression des test d'angularjs

--- app/webroot/js/listeCtrler.js
+++ app/webroot/js/listeCtrler.js
@@ -2,6 +2,5 @@ var list = angular.module('Liste', []);
 
 
 list.controller('listeController', function listeController($scope) {
-	$scope.remaining = 4;
-	console.log($scope.lists);
+	
 });
--- app/webroot/js/tasksCtrler.js
+++ app/webroot/js/tasksCtrler.js
@@ -1,6 +1,5 @@
 var tasks = angular.module('Tasks', []);
 
 tasks.controller('tasksController', function tasksController($scope) {
-	$scope.remaining = 4;
-	console.log($scope.lists);
+	
 });
