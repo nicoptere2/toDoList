bdb4b23442262e1da94a3c4d46f5a2df08afd2ca
Ouvredubonheur <julien.beaugrand@free.fr>
2015-03-28 18:41:22 +0100

    Update User.php

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,10 +1,10 @@
 <?php 
 class User extends AppModel {
-	public $hasMany = array( 'Member' => array('className' => 'Member', 'foreignKey' => 'member_id'),
+	/*public $hasMany = array( 'Member' => array('className' => 'Member', 'foreignKey' => 'member_id'),
 							'Message' => array('className' => 'Message', 'foreignKey' => 'message_id'),
 							'Checked' => array('className' => 'Checked', 'foreignKey' => 'checkedby_id'),
 							'Friend' => array('className' => 'Friend', 'foreignKey' => 'friend_id')
 							);
 
-	public $hasOne = 'Friend';
+	public $hasOne = 'Friend';*/
 }
