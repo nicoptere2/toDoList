47b2ccc6699025fd3f1c0f449be085b21827b8ea
Ouvredubonheur <julien.beaugrand@free.fr>
2015-03-30 16:59:54 +0200

    ajout fonction profil

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,5 +1,19 @@
 <?php 
-class UsersController  extends AppController {
-	public $scaffold;
+class UsersController extends AppController {
+	
+	public function profil()
+	{
+		
+		//$id = $this->Auth->user('id');
+		$id = $this->User->find('all',array(
+		'fields'=>array('username','email','age')));
+		$avertissement = "Vous avez été déconnecté";
+	
+			$this->set('test',$id);	
+	
+		
+	}
+
+ 
 
 }
