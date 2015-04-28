abaf0dd234b5e8c386e0c9ccc394c794b86e261e
unknown <eragon333@hotmail.fr>
2015-04-28 11:39:01 +0200

    fin definitive d'ajouterMebre ?

--- app/Config/database.php
+++ app/Config/database.php
@@ -69,7 +69,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'toor',
+		'password' => '',
 		'database' => 'todolist',
 		'prefix' => '',
 		//'encoding' => 'utf8',
